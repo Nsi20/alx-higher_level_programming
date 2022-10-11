80c733cc0f47afbd9ef382824e12034c8a665462
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2022-10-11 01:15:20 +0100

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a list and only integers"""
+    n = 0
+    for i in range(0, x):
         try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += (a ** b) / i
-        except Exception:
-            result = b + a
-            break
-        return result
+            print("{:d}".format(my_list[i]), end="")
+            n += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+    print()
+    return n
