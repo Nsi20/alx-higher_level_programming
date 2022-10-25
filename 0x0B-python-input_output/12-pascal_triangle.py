b15b0c9aa9b01835445f5b062a806234558f3ddc
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2022-10-25 20:57:59 +0100

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""
-Defines pasacal triangle that create list of lists
-"""
+""" pascal_triangle module """
 
 
-def pascal_triangle(n):
+def pascal_triangle(prmN):
     """
-    defines triangle module
+        Function that define a Pascal triangle
+        Args:
+            prmN: size of the triangle
     """
-    if n <= 0:
-        return []
-    temp = []
-    l = []
-    for i in range(n):
+
+    triangle = []
+    if prmN <= 0:
+        return triangle
+
+    for i in range(prmN):
         row = []
-        for j in range(i + 1):
-            if i == 0 or j == 0 or i == j:
+        for j in range(prmN):
+            if (j > i):
+                break
+            if i > 0 and len(triangle) == i and len(triangle[i - 1]) > j:
+                y = triangle[i - 1][j]
+            elif len(triangle) >= i:
+                y = 0
+            if (
+                i > 0 and len(triangle) > i - 1 and
+                j > 0 and len(triangle[i - 1]) > j - 1
+            ):
+                x = triangle[i - 1][j - 1]
+            else:
+                x = 0
+            if x == 0 and y == 0:
                 row.append(1)
             else:
-                row.append(l[j] + l[j - 1])
-        l = row
-        temp.append(row)
-    return temp
+                row.append(x + y)
+        triangle.append(row)
+
+    return 
