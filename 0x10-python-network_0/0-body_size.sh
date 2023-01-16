7cd2a07780e74aeaaecc7e1674b05646972237e3
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2023-01-16 23:41:40 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
