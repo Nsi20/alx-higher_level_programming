23bc53c7f8dfc0640bc7a76b0fcb9a96d6851934
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2023-01-18 00:06:39 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+
+# Get URL from command line argument URL=$1
+
+# Send request to URL and store response in a variable
+response=$(curl -s "$URL")
+
+# Get the size of the response in bytes
+size=$(echo "$response" | wc -c)
+
+# Display the size in bytes
+echo "Size of response in bytes: $size"
